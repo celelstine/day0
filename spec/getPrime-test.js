4ab07ce2d816b5da158c8499f91df9119e140d3a
celestine <okorocelestine@gmail.com>
2017-04-08 08:48:45 +0100

    Add test script for getPrime

---
+++ spec/getPrime-test.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+  var getprimes = require('../app/library.js');
+
+  describe("Prime Numbers tests ", function() {
+  describe("Case for wrong dataTypes", function() {
+
+    it("should return 'expecting a number' for null", function() {
+      expect(getprimes(null)).toEqual('expecting a number');
+    });
+
+    it("should return 'expecting a number' for 'fake'", function() {
+      expect(getprimes('fake')).toEqual('expecting a number');
+    });
+
+  });
+
+  describe("Case for numbers less than 1", function() {
+
+    it("should return  `expecting only numbers greater than 0` for 0", function() {
+      expect(getprimes(0)).toBe('expecting only numbers greater than 0');
+    });
+
+    it("should return  for `expecting only numbers greater than 0` for -5", function() {
+      expect(getprimes(-5)).toBe('expecting only numbers greater than 0');
+    });
+
+  });
+
+  describe("Case for Natural Numbers", function() {
+
+    it("should return [] for 1", function() {
+      expect(getprimes(1)).toBe([]);
+    });
+
+    it("should return [2,3,5] for 5", function() {
+      expect(getprimes(5)).toBe([2,3,5] );
+    });
+
+    it("should return [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97 ] for 100", function() {
+      expect(getprimes(100)).toBe([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97 ]);
+    });
+
+
+  });
+
+
+});
+
+})();
